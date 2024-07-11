3bf0e320f61d596978db78f9336bf6dedddc1f83
Brandon Duong <bduong1497@gmail.com>
2024-07-10 20:12:38 -0400

    Added username and password to userSchema. Added routes to login and signup to the router

--- models/User.js
+++ models/User.js
@@ -17,6 +17,14 @@ const userSchema = new Schema(
       unique: true,
       match: [/.+@.+\..+/, "Please enter a valid email address"],
     },
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
   {
     timestamps: true,
