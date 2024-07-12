c710cefa079e2593e2a72bac363c02242a8da574
Brandon Duong <bduong1497@gmail.com>
2024-07-12 01:42:31 -0400

    Rendered backend data for full name, email, username, and password

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,28 @@
 import React, { useEffect, useState } from "react";
 
 function App() {
-  const [backendData, setBackendData] = useState(null);
+  const [backendData, setBackendData] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/users")
+    fetch("http://localhost:3000/users/6690b0ea402242d77de3a6f8")
       .then((response) => response.json())
       .then((data) => {
-        setBackendData(data.message);
+        console.log(data);
+        setBackendData(data);
       })
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
-  return <div>{backendData}</div>;
+  return (
+    <>
+      <div>
+        Name: {backendData.firstname} {backendData.lastname}
+      </div>
+      <div>Email: {backendData.email}</div>
+      <div>Username: {backendData.username}</div>
+      <div>Password: {backendData.password}</div>
+    </>
+  );
 }
 
 export default App;
