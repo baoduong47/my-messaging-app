a2fc6e5ba5285df192663a88730fe2e40847e788
Brandon Duong <bduong1497@gmail.com>
2024-07-12 00:38:02 -0400

    organized into separate folders

---
+++ server/models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    firstname: {
+      type: String,
+      default: "",
+    },
+    lastname: {
+      type: String,
+      default: "",
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Please enter a valid email address"],
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
---
+++ server/routes/users.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const userRouter = express.Router();
+const User = require("../models/user");
+
+userRouter.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+userRouter.route("/signup").post(async (req, res) => {
+  const { firstname, lastname, email, username, password } = req.body;
+  const existingUser = await User.findOne({ $or: [{ email }] });
+
+  if (existingUser) {
+    console.log("User already exists", existingUser);
+    return res.status(400).send("User with this email already exists");
+  }
+
+  try {
+    const newUser = new User({
+      firstname,
+      lastname,
+      email,
+      username,
+      password,
+    });
+    newUser.save();
+    res.status(200).send("User created successfully!");
+    console.log(newUser);
+  } catch (error) {
+    return res.status(500).send("Error registering user");
+  }
+});
+
+userRouter.route("/:userId").get(async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    console.log("Received userId", userId);
+
+    const user = await User.findById(userId);
+    console.log("Found user", user);
+
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error); // Log error for debugging
+    return res.status(500).send("Error retrieving user");
+  }
+});
+
+userRouter.get("/login", (req, res, next) => {
+  res.send("Welcome to the Login Page");
+});
+
+module.exports = userRouter;
