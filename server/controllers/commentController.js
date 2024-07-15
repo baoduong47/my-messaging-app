bd6eb01860d5fe49c8039baecdccee84677c0f13
Brandon Duong <bduong1497@gmail.com>
2024-07-15 00:47:00 -0400

    Updated commentController with exports.editComment

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -1,4 +1,5 @@
 const Comment = require("../models/comment");
+const { findById, findByIdAndUpdate } = require("../models/user");
 
 exports.getComments = async (req, res) => {
   try {
@@ -6,6 +7,23 @@ exports.getComments = async (req, res) => {
     res.status(200).json(comments);
   } catch (error) {
     return res.status(500).send("Error retrieving comments");
+  }
+};
+
+exports.getCommentById = async (req, res) => {
+  try {
+    const commentId = req.params.commentId;
+
+    const comment = await Comment.findById(commentId);
+    console.log("Found comment", comment);
+
+    if (!comment) {
+      return res.status(500).send("Comment not found");
+    }
+
+    res.status(200).json(comment);
+  } catch (error) {
+    return res.status(500).send("Error retrieving comment");
   }
 };
 
@@ -24,5 +42,26 @@ exports.postComment = async (req, res) => {
       .json({ message: "Comment saved successfully!", newComment });
   } catch (error) {
     res.status(500).json({ message: "Error saving comment", error });
+  }
+};
+
+exports.editComment = async (req, res) => {
+  try {
+    const commentId = req.params.commentId;
+    const updates = req.body;
+
+    const updatedComment = await Comment.findByIdAndUpdate(commentId, updates, {
+      new: true,
+    });
+
+    if (!updatedComment) {
+      return res.status(404).json({ message: "Comment not found" });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "Comment updated successfully", updatedComment });
+  } catch (error) {
+    return res.status(404).json({ message: "Error updating comment", error });
   }
 };
--- server/routes/commentRouter.js
+++ server/routes/commentRouter.js
@@ -3,9 +3,12 @@ const commentRouter = express.Router();
 
 const {
   getComments,
+  getCommentById,
   postComment,
+  editComment,
 } = require("../controllers/commentController");
 
 commentRouter.route("/").get(getComments).post(postComment);
+commentRouter.route("/:commentId").get(getCommentById).put(editComment);
 
 module.exports = commentRouter;
