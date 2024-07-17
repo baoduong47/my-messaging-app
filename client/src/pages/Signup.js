b786f87b0671a4c38fcd9847ed23ef321135133c
Brandon Duong <bduong1497@gmail.com>
2024-07-17 15:10:13 -0400

    Styled signup page with MaterialUI

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { registerUser } from "../redux/actions/authActions";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import GoogleIcon from "@mui/icons-material/Google";
+import FacebookIcon from "@mui/icons-material/Facebook";
 
 const Signup = () => {
   const [formData, setFormData] = useState({
@@ -34,60 +38,121 @@ const Signup = () => {
         className="w-3/6 flex items-center justify-center bg-cover bg-center"
         style={{ backgroundImage: "url('/images/image.jpg')" }}
       >
-        <h1 className="text-white text-4xl font-bold">ChatBox Messaging App</h1>
+        {/* <h1 className="text-white text-4xl font-bold">ChatBox Messaging App</h1> */}
       </div>
       <div className="w-3/6 bg-white flex items-center justify-center p-8">
         <div className="w-full max-w-md">
-          <h2 className="text-4xl font-bold mb-14">Create Account</h2>
+          <h2 className="text-5xl font-bold mb-14">Create Account</h2>
           <form className="space-y-6" onSubmit={handleSubmit}>
             <div className="flex space-x-4">
-              <input
+              <TextField
                 type="firstname"
                 id="firstname"
                 name="firstname"
-                placeholder="First Name"
                 value={formData.firstname}
                 onChange={handleChange}
                 required
-                className="w-1/2 px-4 py-2 border border-gray-400 rounded-lg"
-              ></input>
-              <input
+                size="small"
+                label="First Name"
+                variant="outlined"
+                className="w-1/2"
+                sx={{
+                  "& .MuiOutlinedInput-root": {
+                    "&.Mui-focused fieldset": {
+                      borderColor: "#023D60",
+                    },
+                    "&:hover fieldset": {
+                      borderColor: "#023D60",
+                    },
+                  },
+                  "& .MuiInputLabel-root.Mui-focused": {
+                    color: "#023D60",
+                  },
+                }}
+              ></TextField>
+              <TextField
                 type="lastname"
                 id="lastname"
                 name="lastname"
-                placeholder="Last Name"
+                size="small"
                 value={formData.lastname}
                 onChange={handleChange}
                 required
-                className="w-1/2 px-4 py-2 border border-gray-400 rounded-lg"
-              ></input>
+                label="Last Name"
+                variant="outlined"
+                className="w-1/2"
+                sx={{
+                  "& .MuiOutlinedInput-root": {
+                    "&.Mui-focused fieldset": {
+                      borderColor: "#023D60",
+                      "&:hover fieldset": {
+                        borderColor: "#023D60",
+                      },
+                    },
+                  },
+                  "& .MuiInputLabel-root.Mui-focused": {
+                    color: "#023D60",
+                  },
+                }}
+              ></TextField>
             </div>
-            <input
+            <TextField
               type="email"
               id="email"
               name="email"
-              placeholder="Email"
               value={formData.email}
               onChange={handleChange}
               required
-              className="w-full px-4 py-2 border border-gray-400 rounded-lg"
-            ></input>
-            <input
+              size="small"
+              label="Email"
+              variant="outlined"
+              fullWidth
+              sx={{
+                "& .MuiOutlinedInput-root": {
+                  "&.Mui-focused fieldset": {
+                    borderColor: "#023D60",
+                    "&:hover fieldset": {
+                      borderColor: "#023D60",
+                    },
+                  },
+                },
+                "& .MuiInputLabel-root.Mui-focused": {
+                  color: "#023D60",
+                },
+              }}
+            ></TextField>
+            <TextField
               type="password"
               id="password"
               name="password"
-              placeholder="Password"
               value={formData.password}
               onChange={handleChange}
               required
-              className="w-full px-4 py-2 border border-gray-400 rounded-lg"
-            ></input>
-            <button
+              size="small"
+              label="Password"
+              variant="outlined"
+              fullWidth
+              sx={{
+                "& .MuiOutlinedInput-root": {
+                  "&.Mui-focused fieldset": {
+                    borderColor: "#023D60",
+                    "&:hover fieldset": {
+                      borderColor: "#023D60",
+                    },
+                  },
+                },
+                "& .MuiInputLabel-root.Mui-focused": {
+                  color: "#023D60",
+                },
+              }}
+            ></TextField>
+            <Button
+              variant="contained"
               type="submit"
               className="w-full px-4 py-2.5 border bg-customBlue text-white rounded-lg flex justify-center items-center"
             >
               Create Account
-            </button>
+            </Button>
             <h4 className="text-xs text-[#666565]">
               Already have an account?{" "}
               <a href="/login" className="text-blue-950">
@@ -100,18 +165,44 @@ const Signup = () => {
               <div className="flex-grow border-t border-gray-400"></div>
             </div>
             <div className="flex space-x-4">
-              <button
+              <Button
+                size="small"
+                variant="outlined"
                 type="button"
-                className="w-1/2 px-4 py-2 border border-gray-400 rounded-lg text-sm text-[#666565]"
+                className="w-1/2"
+                startIcon={<GoogleIcon sx={{ color: "#008744" }} />}
+                sx={{
+                  borderColor: "gray",
+                  color: "#666565",
+                  padding: "10px 16px",
+                  fontSize: "11px",
+                  "&:hover": {
+                    borderColor: "gray",
+                    color: "#666565",
+                  },
+                }}
               >
                 Sign up with Google
-              </button>
-              <button
+              </Button>
+              <Button
+                size="small"
+                variant="outlined"
                 type="button"
-                className="w-1/2 px-4 py-2 border border-gray-400 rounded-lg text-sm text-[#666565]"
+                className="w-1/2"
+                startIcon={<FacebookIcon sx={{ color: "#3B5998" }} />}
+                sx={{
+                  borderColor: "gray",
+                  color: "#666565",
+                  padding: "10px 16px",
+                  fontSize: "11px",
+                  "&:hover": {
+                    borderColor: "gray",
+                    color: "#666565",
+                  },
+                }}
               >
                 Sign up with Facebook
-              </button>
+              </Button>
             </div>
           </form>
         </div>
