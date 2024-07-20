f6be9cf905904fd06b1aef649a58608d9ff28b68
Brandon Duong <bduong1497@gmail.com>
2024-07-20 03:28:56 -0400

    Loaded picture in profile.js

--- server/app.js
+++ server/app.js
@@ -38,6 +38,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(cors());
 
 app.use(cors({ origin: "http://localhost:3001" }));
